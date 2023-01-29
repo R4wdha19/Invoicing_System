4a513676e2268fc9f1ba3e48a1ce5864ef702c9f
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2023-01-29 20:29:51 +0400

    Added A Menu For Items Management

--- src/ShopItemManagement.java
+++ src/ShopItemManagement.java
@@ -1,2 +1,10 @@
 public class ShopItemManagement {
+    public static void manageShopMenu(){
+        System.out.println("What Would Like To Manage ?");
+        System.out.println("1 - Add An Item ");
+        System.out.println("2 - Delete An Item ");
+        System.out.println("3 - Change The Price Of An Item");
+        System.out.println("4 - Get A Report Of All Items ");
+        System.out.println("5 - Go Back To Main Menu ");
+    }
 }
