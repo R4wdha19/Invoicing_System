7f100faa137d895599495a7f860b72220fd0056c
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2023-01-29 23:35:43 +0400

    Main Menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(" Welcome To Our Invoicing System, These Are The Services That Our System Provide:");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
     }
 }
