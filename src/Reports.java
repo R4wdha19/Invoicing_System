a3763594b1b7ec526b3de34a4a46b3575e78992b
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2023-01-29 20:38:53 +0400

    Updated Reports class

--- src/Reports.java
+++ src/Reports.java
@@ -1,2 +1,10 @@
 public class Reports {
+    public static void reportsMenu(){
+        System.out.println(" What Report You Would Like To See ? ");
+        System.out.println(" 1 - The Shop Statistics ");
+        System.out.println(" 2 - All The Invoices ");
+        System.out.println(" 3 - All Shop Customers ");
+        System.out.println(" 4 - All Shop Employees");
+        System.out.println(" 5 - Go Back To Main Menu ");
+    }
 }
