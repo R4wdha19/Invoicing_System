bda383e4ba81608dc1dcb7090ef4d79af05169e5
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2023-01-29 14:59:02 +0400

    Added menu to shop settings class

---
+++ src/Invoices.java
@@ -0,0 +1,2 @@
+public class Invoices {
+}
--- src/ShopSettings.java
+++ src/ShopSettings.java
@@ -1,2 +1,10 @@
 public class ShopSettings {
+    public static void shopSettingsMenu(){
+        System.out.println("Here Are The Settings Of Our System, Please Choose What You Want To Do :");
+        System.out.println(" 1 - Add A New Shop ");
+        System.out.println(" 2 - Load The Shop Data ");
+        System.out.println(" 3 - Set The Invoice Header ");
+        System.out.println(" 4 - Go Back To The Main Menu");
+    }
+
 }
