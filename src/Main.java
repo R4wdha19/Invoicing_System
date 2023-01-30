27c21323127a76f2d3ea0603b88f4c604262d6e1
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2023-01-30 09:49:05 +0400

    Updated Main Menu

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(" Welcome To Our Invoicing System, These Are The Services That Our System Provide:");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-    }
+	public static void main(String[] args) {
+		System.out.println(" Welcome To Our Invoicing System, These Are The Services That Our System Provide:");
+		System.out.println(" 1 - Shop Settings");
+		System.out.println(" 2 - Shop Management ");
+		System.out.println(" 3 - Create A New Invoice ");
+		System.out.println(" 4 - Get Reports ");
+		System.out.println(" 5 - Search An Invoice ");
+		System.out.println(" 6 - Program Statistics ");
+		System.out.println(" 7 - Exit ");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+	}
 }
