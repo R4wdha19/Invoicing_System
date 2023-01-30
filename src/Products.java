67fecaa6f2d78cf07ac67ad63ff26b339a8d9f97
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2023-01-30 15:54:32 +0400

    Update Products.java

--- src/Products.java
+++ src/Products.java
@@ -1,16 +1,64 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Products {
+	public static final String url = "";
+	public static final String user = "";
+	public static final String pass = "";
+	public static Connection con;
+	public static Scanner scanner = new Scanner(System.in);
+
+	public static void establishADataBaseConnection() {
+
+		System.out.println("To Start Saving Your Data, We Need To Connect To The Data Base,"
+				+ "/t /n Help Us By Providing The Following");
+		System.out.println(" What Is The Name Of Your DataBase ?");
+		String dataBaseName = scanner.next();
+		System.out.println(" What Is The UserName Of Your DataBase ?");
+		String dataBaseUserName = scanner.next();
+		System.out.println(" What Is The PassWord Of Your DataBase ?");
+		String dataBasePassWord = scanner.next();
+		String url = "jdbc:sqlserver://localhost:1433;databaseName=" + dataBaseName
+				+ ";encrypt=true;trustServerCertificate=true";
+		String user = dataBaseUserName;
+		String pass = dataBasePassWord;
+
+		try {
+			con = DriverManager.getConnection(url, user, pass);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void closingConnection() {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 	public static void productsDetails() {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println(" What Is The Product Name ?");
-		String productName = scanner.next();
-		System.out.println(" What Is The Product Price ?");
-		int productPrice = scanner.nextInt();
-		System.out.println(" What Is The Proudct Available Quantity ?");
-		int productAvailableQuantity = scanner.nextInt();
-		System.out.println(" What Is The Quantity Price ?");
-		int productQuantityPrice = scanner.nextInt();
+
+		System.out.println(" Enter The Number Of Products You Would Like To Add ");
+		int numberOfProducts = scanner.nextInt();
+		for (int i = 0; i < numberOfProducts; i++) {
+			System.out.println(" What Is The Product Name ?");
+			String productName = scanner.next();
+			System.out.println(" What Is The Product Price ?");
+			int productPrice = scanner.nextInt();
+			System.out.println(" What Is The Proudct Available Quantity ?");
+			int productAvailableQuantity = scanner.nextInt();
+			System.out.println(" What Is The Quantity Price ?");
+			int productQuantityPrice = scanner.nextInt();
+
+			String sqlQueryToInsert = "  INSERT INTO Items (itemName,unitPrice,quantity,quantityAmount)" + " VALUES ( '"
+					+ productName + "'," + " ' " + productPrice + "','" + productAvailableQuantity + "'" + ",'"
+					+ productQuantityPrice + "'" + ") ";
+		}
+
 	}
 
 }
