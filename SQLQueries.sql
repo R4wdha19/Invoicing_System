aaed63e6d3d3db8bb51d731e4b9efb15b8f5ba00
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2023-01-30 18:02:42 +0400

    Update SQLQueries.sql

--- SQLQueries.sql
+++ SQLQueries.sql
@@ -15,3 +15,18 @@ itemName VARCHAR(600),
 unitPrice int,
 quantity int,
 quantityAmount int);
+
+
+create table shop(
+shopId integer PRIMARY KEY IDENTITY(1,1),
+shopName VARCHAR(100),
+shopLocation VARCHAR(100),
+shopContactNumber int,
+shopFaxNumber int,
+shopEmail VARCHAR(100),
+shopWebsite VARCHAR(100));
+
+
+select * from Invoice;
+select * from Items;
+select * from shop;
