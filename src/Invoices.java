a05463be36f8d7d123cbf8cfe4f16ca12619b131
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2023-01-30 11:17:35 +0400

    Update Invoices.java

--- src/Invoices.java
+++ src/Invoices.java
@@ -3,7 +3,16 @@ import java.util.Scanner;
 public class Invoices {
 	public static void shopInvoicesDetails() {
 		Scanner scanner = new Scanner(System.in);
-		String shopeName = scanner.next();
+		System.out.println(" Whate Is The Name Of The Shop ? ");
+		String shopName = scanner.next();
+		System.out.println(" Whate Is The Shop Contact Number ? ");
+		int shopContactNumber = scanner.nextInt();
+		System.out.println(" Whate Is The Shop Website ? ");
+		String shopWebsite = scanner.next();
+		System.out.println(" Whate Is The Shop Email ? ");
+		String shopEmail = scanner.next();
+		System.out.println(" What Is The Shop Fax Number ?");
+		int shopFax = scanner.nextInt();
 	}
 
 }
