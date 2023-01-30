23695802b5d8fcf6149add0dfd4aec2ba7b8c12a
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2023-01-30 11:33:27 +0400

    Update Products.java

--- src/Products.java
+++ src/Products.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
 
 public class Products {
+	public static void productsDetails() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(" What Is The Product Name ?");
+		String productName = scanner.next();
+		System.out.println(" What Is The Product Price ?");
+		int productPrice = scanner.nextInt();
+		System.out.println(" What Is The Proudct Available Quantity ?");
+		int productAvailableQuantity = scanner.nextInt();
+		System.out.println(" What Is The Quantity Price ?");
+		int productQuantityPrice = scanner.nextInt();
+	}
 
 }
