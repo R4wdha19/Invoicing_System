cbc9d2dbdf01d07645a4cc6b69dfe7b1d5daedb9
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2023-01-31 10:32:10 +0400

    Update SQLQueries.sql

--- SQLQueries.sql
+++ SQLQueries.sql
@@ -1,17 +1,24 @@
 create database Invoices ;
 use Invoices;
+
+
 create table Invoice (
-customername VARCHAR(500),
-phoneNumber VARCHAR(500),
- invoiceDate date,
- numberOfItems int,
- totalAmount int, 
+InvoiceId integer PRIMARY KEY IDENTITY(1,1),
+CustomerId int references Customers(CustomerId),
+invoiceDate date,
+numberOfItems int,
+totalAmount int, 
 paidAmount int,
-balance int);
+balance int,
+shopContactNumber int,
+shopFaxNumber int,
+shopEmail VARCHAR(100),
+shopWebsite VARCHAR(100));
 
 create table Items(
 itemId integer PRIMARY KEY IDENTITY(1,1),
-itemName VARCHAR(600),
+InvoiceId int references Invoice(InvoiceId),
+itemName VARCHAR(50),
 unitPrice int,
 quantity int,
 quantityAmount int);
@@ -27,6 +34,22 @@ shopEmail VARCHAR(100),
 shopWebsite VARCHAR(100));
 
 
+
+create table Customers (
+CustomerId integer PRIMARY KEY IDENTITY(1,1),
+customername VARCHAR(100),
+customerphoneNumber VARCHAR(50),
+customerPaymentMethod VARCHAR(20),
+);
+
+
+
+
 select * from Invoice;
 select * from Items;
 select * from shop;
+select * from Customers;
+drop table Invoice;
+drop table Items;
+drop table shop;
+
